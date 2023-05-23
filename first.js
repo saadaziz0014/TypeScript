7eee956379ad88a170aba94932a8a3804da06476
saadaziz0014 <saadaziz0014@gmail.com>
2023-05-23 20:54:42 +0500

    first_half_hour

---
+++ first.js
@@ -0,0 +1,9 @@
+// type safety
+// static check
+var obj = {
+    name: "saad",
+    age: 22
+};
+//console.log(obj.email); //email show error here 
+var myName = "saad";
+console.log(myName);
---
+++ first.ts
@@ -0,0 +1,17 @@
+// type safety
+
+// static check
+
+let obj = {
+    name:"saad",
+    age:22
+};
+
+//console.log(obj.email); //email show error here 
+
+
+let myName:string = "saad";
+
+console.log(myName);
+
+
